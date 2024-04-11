669fa398c06cebf739d29c10da0d59656127afef
mmorudi023 <mmorudi023@student.wethinkcode.co.za>
2024-04-11 16:13:56 +0200

    added a console.py file

---
+++ console.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Console module"""
+
+
+import cmd
+
+
+class HBNBCmd(cmd.Cmd):
+    """Console class for interactive shell"""
+
+    prompt = "(hbnb) "
+
+    def do_EOF(self, arg):
+        """exits the program"""
+        return True
+
+    def do_quit(self, arg):
+        """ exits the shell """
+        return True
+
+if __name__ == "__main__":
+    HBNBCmd().cmdloop()
