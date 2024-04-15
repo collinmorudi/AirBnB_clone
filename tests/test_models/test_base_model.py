fa35b6747b2e58be4eca980ca6710f66f519034b
mmorudi023 <mmorudi023@student.wethinkcode.co.za>
2024-04-15 13:56:40 +0200

    fixing pep8 style issues

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -26,13 +26,13 @@ class TestBaseModel(unittest.TestCase):
     def test_to_dict(self):
         model_obj = BaseModel()
         self.assertTrue(isinstance(model_obj.to_dict(), dict))
-        
         model_dict = model_obj.to_dict()
         self.assertEqual(model_dict["__class__"], "BaseModel")
         self.assertEqual(model_dict["id"], model_obj.id)
-        self.assertEqual(model_dict["created_at"], model_obj.created_at.isoformat())
-        self.assertEqual(model_dict["updated_at"], model_obj.updated_at.isoformat())
-
+        self.assertEqual(model_dict["created_at"],
+                         model_obj.created_at.isoformat())
+        self.assertEqual(model_dict["updated_at"],
+                         model_obj.updated_at.isoformat())
 
     def test_str(self):
         model_obj = BaseModel()
@@ -40,6 +40,6 @@ class TestBaseModel(unittest.TestCase):
         self.assertIn(str(model_obj.__dict__), str(model_obj))
         self.assertIn(model_obj.id, str(model_obj))
 
-    
+
 if __name__ == "__main__":
     unittest.main()
